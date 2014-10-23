932a3d79c31353d70bb612e20d1ccba8a0e037e2
Alexander Graefe <alex@codemo.de>
2014-10-23 10:46:30 +0200

    Deleting jobs also deletes their uniqueness lock

---
+++ lib/sidekiq_unique_ext.rb
@@ -0,0 +1,27 @@
+module Sidekiq
+  class Job
+    module UniqueExtension
+      def self.included(base)
+        base.class_eval do
+          alias_method :delete_orig, :delete
+          alias_method :delete, :delete_ext
+        end
+      end
+
+      def delete_ext
+        unlock(payload_hash(self.item))
+        delete_orig
+      end
+      protected
+
+      def payload_hash(item)
+        SidekiqUniqueJobs::PayloadHelper.get_payload(item['class'], item['queue'], item['args'])
+      end
+
+      def unlock(payload_hash)
+        Sidekiq.redis { |conn| conn.del(payload_hash) }
+      end
+    end
+    include UniqueExtension
+  end
+end
---
+++ spec/lib/sidekiq_unique_ext_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'sidekiq/api'
+require 'sidekiq/worker'
+require 'sidekiq-unique-jobs/middleware/server/unique_jobs'
+require 'sidekiq-unique-jobs/middleware/client/unique_jobs'
+require 'sidekiq_unique_ext'
+
+class JustAWorker
+  include Sidekiq::Worker
+
+  sidekiq_options unique: true, queue: 'testqueue'
+
+  def perform
+  end
+end
+
+describe Sidekiq::Job::UniqueExtension do
+
+
+  before do
+    Sidekiq.redis = REDIS
+    Sidekiq.redis {|c| c.flushdb }
+  end
+
+  it 'deletes uniqueness lock on delete' do
+    params = {:foo => "bar"}
+    payload_hash = SidekiqUniqueJobs::PayloadHelper.get_payload("JustAWorker", "testqueue", [params])
+    jid = JustAWorker.perform_async(:foo => "bar")
+    queue = Sidekiq::Queue.new("testqueue")
+    job = queue.find_job(jid)
+    job.delete
+    Sidekiq.redis do |c|
+      expect(c.exists(payload_hash)).to be_falsy
+    end
+  end
+end
