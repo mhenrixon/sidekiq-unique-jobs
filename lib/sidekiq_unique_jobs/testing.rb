606298f29c33eb048eaca5e016108ae87bb432a8
Mikael Henriksson <mikael@zoolutions.se>
2014-11-18 15:37:00 +0100

    Add more descriptive failure message

--- lib/sidekiq_unique_jobs/testing.rb
+++ lib/sidekiq_unique_jobs/testing.rb
@@ -1,4 +1,10 @@
-require 'mock_redis'
+begin
+  require 'mock_redis'
+rescue
+  raise 'To test using Sidekiq::Testing.inline!' \
+        ' Please add `gem "mock_redis" to your gemfile.'
+end
+
 module SidekiqUniqueJobs
   def self.redis_mock
     @redis_mock ||= MockRedis.new
