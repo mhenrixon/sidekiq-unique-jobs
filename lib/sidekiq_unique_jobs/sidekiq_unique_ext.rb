732f34385bc2dcd7099ebe087917376a199e3de7
Mikael Henriksson <mikael@zoolutions.se>
2015-08-30 09:20:10 +0200

    Close #97

--- lib/sidekiq_unique_jobs/sidekiq_unique_ext.rb
+++ lib/sidekiq_unique_jobs/sidekiq_unique_ext.rb
@@ -2,11 +2,41 @@ require 'sidekiq/api'
 require 'sidekiq_unique_jobs/server/extensions'
 
 module Sidekiq
+  class SortedEntry
+    module UniqueExtension
+      include SidekiqUniqueJobs::Server::Extensions
+
+      def self.included(base)
+        base.class_eval do
+          alias_method :delete_orig, :delete
+          alias_method :delete, :delete_ext
+          alias_method :remove_job_orig, :remove_job
+          alias_method :remove_job, :remove_job_ext
+        end
+      end
+
+      def delete_ext
+        unlock(payload_hash(item), item)
+        delete_orig
+      end
+
+      def remove_job_ext
+        remove_job_orig do |message|
+          unlock(payload_hash(Sidekiq.load_json(message)), item)
+          yield message
+        end
+      end
+    end
+    include UniqueExtension
+  end
+
   class Job
     module UniqueExtension
+      SCHEDULED ||= 'schedule'.freeze
       include SidekiqUniqueJobs::Server::Extensions
       extend Forwardable
       def_delegator :SidekiqUniqueJobs, :payload_hash
+
       def self.included(base)
         base.class_eval do
           alias_method :delete_orig, :delete
@@ -24,7 +54,7 @@ module Sidekiq
       def unlock(lock_key, item)
         Sidekiq.redis do |con|
           con.eval(remove_on_match, keys: [lock_key], argv: [item['jid']])
-          if @parent && @parent.name == 'schedule'.freeze
+          if defined?(@parent) && @parent && @parent.name == SCHEDULED
             con.eval(remove_scheduled_on_match, keys: [lock_key], argv: [item['jid']])
           else
             con.eval(remove_on_match, keys: [lock_key], argv: [item['jid']])
--- spec/lib/sidekiq_unique_ext_spec.rb
+++ spec/lib/sidekiq_unique_ext_spec.rb
@@ -5,66 +5,72 @@ require 'sidekiq_unique_jobs/server/middleware'
 require 'sidekiq_unique_jobs/client/middleware'
 require 'sidekiq_unique_jobs/sidekiq_unique_ext'
 
-class JustAWorker
-  include Sidekiq::Worker
+RSpec.describe 'Sidekiq::Api' do
+  class JustAWorker
+    include Sidekiq::Worker
 
-  sidekiq_options unique: true, queue: 'testqueue'
+    sidekiq_options unique: true, queue: 'testqueue'
 
-  def perform
-  end
-end
-
-RSpec.describe Sidekiq::Job::UniqueExtension do
-  before do
-    Sidekiq.redis = REDIS
-    Sidekiq.redis(&:flushdb)
-  end
-
-  it 'deletes uniqueness lock on delete' do
-    params = { foo: 'bar' }
-    payload_hash = SidekiqUniqueJobs.get_payload('JustAWorker', 'testqueue', [params])
-    jid = JustAWorker.perform_async(foo: 'bar')
-    queue = Sidekiq::Queue.new('testqueue')
-    job = queue.find_job(jid)
-    job.delete
-    Sidekiq.redis do |c|
-      expect(c.exists(payload_hash)).to be_falsy
+    def perform
     end
   end
-end
 
-RSpec.describe Sidekiq::Queue::UniqueExtension do
   before do
     Sidekiq.redis = REDIS
     Sidekiq.redis(&:flushdb)
   end
 
-  it 'deletes uniqueness locks on clear' do
-    params = { foo: 'bar' }
-    payload_hash = SidekiqUniqueJobs.get_payload('JustAWorker', 'testqueue', [params])
+  let(:params) { { foo: 'bar' } }
+  let(:payload_hash) { SidekiqUniqueJobs.get_payload('JustAWorker', 'testqueue', [params]) }
+
+  def schedule_job
+    JustAWorker.perform_in(60 * 60 * 3, params)
+  end
+
+  def perform_async
     JustAWorker.perform_async(foo: 'bar')
-    queue = Sidekiq::Queue.new('testqueue')
-    queue.clear
-    Sidekiq.redis do |c|
-      expect(c.exists(payload_hash)).to be_falsy
+  end
+
+  describe Sidekiq::SortedEntry::UniqueExtension do
+    it 'deletes uniqueness lock on delete' do
+      schedule_job
+
+      Sidekiq::ScheduledSet.new.each(&:delete)
+      Sidekiq.redis do |c|
+        expect(c.exists(payload_hash)).to be_falsy
+      end
+
+      expect(schedule_job).not_to eq(nil)
     end
   end
-end
 
-RSpec.describe Sidekiq::JobSet::UniqueExtension, sidekiq_ver: 3 do
-  before do
-    Sidekiq.redis = REDIS
-    Sidekiq.redis(&:flushdb)
+  describe Sidekiq::Job::UniqueExtension do
+    it 'deletes uniqueness lock on delete' do
+      jid = perform_async
+      Sidekiq::Queue.new('testqueue').find_job(jid).delete
+      Sidekiq.redis do |c|
+        expect(c.exists(payload_hash)).to be_falsy
+      end
+    end
   end
 
-  it 'deletes uniqueness locks on clear' do
-    params = { foo: 'bar' }
-    payload_hash = SidekiqUniqueJobs.get_payload('JustAWorker', 'testqueue', [params])
-    JustAWorker.perform_in(60 * 60 * 3, foo: 'bar')
-    set = Sidekiq::JobSet.new('schedule')
-    set.clear
-    Sidekiq.redis do |c|
-      expect(c.exists(payload_hash)).to be_falsy
+  describe Sidekiq::Queue::UniqueExtension do
+    it 'deletes uniqueness locks on clear' do
+      perform_async
+      Sidekiq::Queue.new('testqueue').clear
+      Sidekiq.redis do |c|
+        expect(c.exists(payload_hash)).to be_falsy
+      end
+    end
+  end
+
+  describe Sidekiq::JobSet::UniqueExtension, sidekiq_ver: 3 do
+    it 'deletes uniqueness locks on clear' do
+      schedule_job
+      Sidekiq::JobSet.new('schedule').clear
+      Sidekiq.redis do |c|
+        expect(c.exists(payload_hash)).to be_falsy
+      end
     end
   end
 end
