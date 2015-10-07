be3362ad5538aee44d9a4c35cdec67655ff6e7bc
Mikael Henriksson <mikael@zoolutions.se>
2015-10-07 12:54:44 +0200

    Fix typos
    Close #119

--- spec/workers/run_lock_with_retries_worker.rb
+++ spec/workers/run_lock_with_retries_worker.rb
@@ -2,7 +2,7 @@ class RunLockWithRetriesWorker
   include Sidekiq::Worker
 
   sidekiq_options unique: true,
-                  unique_locks: :while_executing,
+                  unique_lock: :while_executing,
                   queue: :unlock_ordering,
                   run_lock_retries: 10,
                   run_lock_retry_interval: 0,
