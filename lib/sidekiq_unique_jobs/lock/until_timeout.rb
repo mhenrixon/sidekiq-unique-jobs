c92f4277ed1fe060e36e21c84f0adcaaf1220c0e
Mikael Henriksson <mikael@zoolutions.se>
2015-11-07 12:15:19 +0100

    Reduce number of created strings

--- lib/sidekiq_unique_jobs/lock/until_timeout.rb
+++ lib/sidekiq_unique_jobs/lock/until_timeout.rb
@@ -2,8 +2,11 @@ module SidekiqUniqueJobs
   module Lock
     class UntilTimeout < UntilExecuted
       def unlock(scope)
-        return true if scope.to_sym == :server
-        fail ArgumentError, "#{scope} middleware can't #{__method__} #{unique_key}"
+        if scope.to_sym == :server
+          return true
+        else
+          fail ArgumentError, "#{scope} middleware can't #{__method__} #{unique_key}"
+        end
       end
 
       def execute(_callback)
