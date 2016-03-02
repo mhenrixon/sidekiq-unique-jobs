ee3cdce8118132b5b77bd7f0cacbea87614f81b4
Mikael Henriksson <mikael@zoolutions.se>
2016-03-02 17:26:14 +0100

    Don't care if the lock was released
    It could have been a timeout so just unlock and return success.

--- redis/release_lock.lua
+++ redis/release_lock.lua
@@ -4,10 +4,9 @@ local stored_jid = redis.pcall('get', unique_key)
 
 if stored_jid then
   if stored_jid == job_id or stored_jid == '2' then
-    if redis.pcall('del', unique_key) then
-      redis.pcall('hdel', 'uniquejobs', job_id)
-      return 1
-    end
+    redis.pcall('del', unique_key)
+    redis.pcall('hdel', 'uniquejobs', job_id)
+    return 1
   else
     return 0
   end
