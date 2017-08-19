7b360088a86d79ff0c4bc616240dcc5bfb6b97d0
Mikael Henriksson <mikael@zoolutions.se>
2017-08-19 15:19:41 +0200

    Clean up more tests

--- spec/lib/sidekiq_unique_jobs/run_lock_timeout_calculator_spec.rb
+++ spec/lib/sidekiq_unique_jobs/run_lock_timeout_calculator_spec.rb
@@ -3,36 +3,42 @@
 require 'spec_helper'
 
 RSpec.describe SidekiqUniqueJobs::RunLockTimeoutCalculator do
-  shared_context 'generic unscheduled job' do
-    subject { described_class.new('class' => 'JustAWorker') }
-  end
+  subject { calculator }
 
-  describe 'public api' do
-    it_behaves_like 'generic unscheduled job' do
-      it { is_expected.to respond_to(:seconds) }
-    end
-  end
+  let(:calculator) { described_class.new(args) }
+  let(:args)       { { 'class' => 'JustAWorker' } }
+
+  it { is_expected.to respond_to(:seconds) }
 
   describe '.for_item' do
+    subject { described_class.for_item('WAT') }
+
     it 'initializes a new calculator' do
       expect(described_class).to receive(:new).with('WAT')
-      described_class.for_item('WAT')
+      subject
     end
   end
 
   describe '#seconds' do
-    context 'using default run_lock_expiration' do
-      subject { described_class.new(nil) }
-      before { allow(subject).to receive(:worker_class_run_lock_expiration).and_return(9) }
+    subject { calculator.seconds }
 
-      its(:seconds) { is_expected.to eq(9) }
+    before do
+      allow(calculator).to receive(:worker_class_run_lock_expiration)
+        .and_return(expiration)
     end
 
-    context 'using specified sidekiq option run_lock_expiration' do
-      subject { described_class.new(nil) }
-      before { allow(subject).to receive(:worker_class_run_lock_expiration).and_return(nil) }
+    context 'when worker_class_run_lock_expiration is configured' do
+      let(:args)       { nil }
+      let(:expiration) { 9 }
 
-      its(:seconds) { is_expected.to eq(60) }
+      it { is_expected.to eq(9) }
+    end
+
+    context 'when worker_class_run_lock_expiration is not configured' do
+      let(:args)       { nil }
+      let(:expiration) { nil }
+
+      it { is_expected.to eq(60) }
     end
   end
 end
