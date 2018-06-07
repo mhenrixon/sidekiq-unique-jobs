5a05322915ebd96469bd00fa4897d35089327737
Mikael Henriksson <mikael@zoolutions.se>
2018-06-07 18:09:08 +0200

    Fix another broken spec

--- spec/lib/sidekiq_unique_jobs/cli_spec.rb
+++ spec/lib/sidekiq_unique_jobs/cli_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 require 'thor/runner'
 
-RSpec.describe SidekiqUniqueJobs::Cli, ruby_ver: '>= 2.4' do
+RSpec.describe SidekiqUniqueJobs::Cli, ruby_ver: '>= 2.5.1' do
   describe '#help' do
     let(:output) { capture(:stdout) { described_class.start(%w[help]) } }
     let(:banner) do
