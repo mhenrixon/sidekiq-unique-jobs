3cdd63637e0039e2648eca38045dd328581ad2bd
Mikael Henriksson <mikael@zoolutions.se>
2018-06-14 20:36:24 +0200

    Require dependency as argument

--- lib/sidekiq_unique_jobs/lock/queue_lock_base.rb
+++ lib/sidekiq_unique_jobs/lock/queue_lock_base.rb
@@ -7,7 +7,7 @@ module SidekiqUniqueJobs
 
       def initialize(item, redis_pool = nil)
         @calculator = SidekiqUniqueJobs::Timeout::QueueLock.new(item)
-        @item       = prepare_item(item)
+        @item       = prepare_item(item, @calculator)
         @redis_pool = redis_pool
         @lock       = SidekiqUniqueJobs::Lock.new(@item, @redis_pool)
       end
