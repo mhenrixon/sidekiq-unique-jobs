f265d7de8ab853fd271eaae83ee653c3484fb38c
Mikael Henriksson <mikael@zoolutions.se>
2018-06-14 20:36:58 +0200

    Adds coverage

---
+++ spec/lib/sidekiq_unique_jobs/lock/run_lock_base_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe SidekiqUniqueJobs::Lock::RunLockBase do
+  let(:lock)       { described_class.new(item, redis_pool) }
+  let(:item)       { {} }
+  let(:redis_pool) { nil }
+
+  describe '#lock' do
+    it do
+      expect { lock.lock(nil) }
+        .to raise_error(NotImplementedError, "#lock needs to be implemented in #{described_class}")
+    end
+  end
+
+  describe '#execute' do
+    it do
+      expect { lock.execute(nil) }
+        .to raise_error(NotImplementedError, "#execute needs to be implemented in #{described_class}")
+    end
+  end
+
+  describe '#unlock' do
+    it do
+      expect { lock.unlock(nil) }
+        .to raise_error(NotImplementedError, "#unlock needs to be implemented in #{described_class}")
+    end
+  end
+end
