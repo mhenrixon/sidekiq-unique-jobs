92e9c91202b3616e254ca7842640e2c054efcd21
Mikael Henriksson <mikael@zoolutions.se>
2018-06-17 20:00:36 +0200

    Add tests

---
+++ spec/unit/sidekiq_unique_jobs/lock/until_executing_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SidekiqUniqueJobs::Lock::UntilExecuting do
+  include_context 'with a stubbed locksmith'
+
+  let(:item) do
+    { 'jid' => 'maaaahjid',
+      'class' => 'UntilTimeoutJob',
+      'unique' => 'until_timeout' }
+  end
+  let(:empty_callback) { -> {} }
+
+  describe '#execute' do
+    it 'calls the callback' do
+      expect(lock).to receive(:unlock).ordered
+      expect(empty_callback).to receive(:call)
+      expect { |block| lock.execute(empty_callback, &block) }.to yield_control
+    end
+  end
+end
