075bc677218b09c27824c4f58217870aff78de52
Mikael Henriksson <mikael@zoolutions.se>
2018-06-26 22:29:32 +0200

    Remove the unlimited hash

--- redis/delete.lua
+++ redis/delete.lua
@@ -2,10 +2,11 @@ local exists_key    = KEYS[1]
 local grabbed_key   = KEYS[2]
 local available_key = KEYS[3]
 local version_key   = KEYS[4]
-local unique_digest = KEYS[5] -- TODO: Legacy support (Remove in v6.1)
+local unique_digest = KEYS[5]     -- TODO: Legacy support (Remove in v6.1)
 
 redis.call('DEL', exists_key)
 redis.call('DEL', grabbed_key)
 redis.call('DEL', available_key)
 redis.call('DEL', version_key)
-redis.call('DEL', unique_digest) -- TODO: Legacy support (Remove in v6.1)
+redis.call('DEL', 'uniquejobs')   -- TODO: Old job hash, just drop the darn thing
+redis.call('DEL', unique_digest)  -- TODO: Legacy support (Remove in v6.1)
