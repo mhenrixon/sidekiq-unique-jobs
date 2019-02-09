f50a6f44089dde5245c65763ab76c66e0e6ba349
Ivan Denisov <johnny.denisov@gmail.com>
2019-02-09 21:25:57 +0200

    add integration test for delete_all endpoint

--- spec/integration/sidekiq_unique_jobs/web_spec.rb
+++ spec/integration/sidekiq_unique_jobs/web_spec.rb
@@ -65,4 +65,22 @@ RSpec.describe SidekiqUniqueJobs::Web, redis: :redis do
 
     expect(SidekiqUniqueJobs::Digests.all).to match_array([another_digest])
   end
+
+  it 'can delete all digests' do
+    expect(MyUniqueJob.perform_async(1, 2)).not_to eq(nil)
+    expect(MyUniqueJob.perform_async(2, 3)).not_to eq(nil)
+
+    expect(SidekiqUniqueJobs::Digests.all).to match_array(expected_digests)
+
+    get "/unique_digests/delete_all"
+    expect(last_response.status).to eq(302)
+
+    follow_redirect!
+
+    expect(last_request.url).to end_with('/unique_digests')
+    expect(last_response.body).not_to match("/unique_digests/#{digest}")
+    expect(last_response.body).not_to match("/unique_digests/#{another_digest}")
+
+    expect(SidekiqUniqueJobs::Digests.all).to be_empty
+  end
 end
