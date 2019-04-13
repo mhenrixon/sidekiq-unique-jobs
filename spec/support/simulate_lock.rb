a06022ba9b98d8dd2dd06f9049f5f55c0b542b1a
Mika Hel <mikael@zoolutions.se>
2019-04-13 19:23:08 +0200

    Keep working away at lock simulation

--- spec/support/simulate_lock.rb
+++ spec/support/simulate_lock.rb
@@ -5,36 +5,44 @@ module SimulateLock
   @items = Concurrent::Array.new
 
   def lock_until_executed(digest, jid, ttl = nil)
-    lock()
-  end
-
-  def lock_until_expired(digest, jid, ttl)
-    item = get_item(digest: digest, jid: jid, lock_type: :until_expired, expiration: ttl)
+    item = get_item(digest: digest, jid: jid, lock_type: :until_executed, ttl: ttl)
     lock(item)
   end
 
-  def lock_until_and_while_executing(digest, jid, ttl)
-    item = get_item(digest: digest, jid: jid, lock_type: :until_expired, expiration: ttl)
+  def lock_until_expired(digest, jid, ttl)
+    item = get_item(digest: digest, jid: jid, lock_type: :until_expired, ttl: ttl)
+    lock(item)
+  end
+
+  def lock_until_and_while_executing(digest, jid, ttl = nil)
+    item = get_item(digest: digest, jid: jid, lock_type: :until_expired, ttl: ttl)
     lock(item)
   end
 
   def lock_while_executing(digest, jid, ttl = nil)
-    lock(digest: digest, jid: "randomjid", lock_type: :while_executing, expiration: nil)
+    item = get_item(digest: digest, jid: "randomjid", lock_type: :while_executing, ttl: nil)
+    lock(item)
   end
 
   def runtime_lock(digest, jid, ttl = nil)
-
+    item = get_item(digest: digest, jid: "randomjid", lock_type: :while_executing, ttl: nil)
+    item = get_item(digest: "#{digest}:RUN", jid: "randomjid", lock_type: :while_executing, ttl: nil)
+    lock(item)
   end
 
   def lock(item)
     Locksmith.new(item).lock
   end
 
-  def get_item(digest: "randomdigest", jid: "randomjid", lock_type: :until_executed, expiration: nil)
+  def unlock(item)
+    Locksmith.new(item).unlock
+  end
+
+  def get_item(digest: "randomdigest", jid: "randomjid", lock_type: :until_executed, ttl: nil)
     item = {
       UNIQUE_DIGEST_KEY => digest,
       JID_KEY => jid,
-      LOCK_EXPIRATION_KEY => expiration,
+      LOCK_EXPIRATION_KEY => ttl,
       LOCK_KEY => lock_type,
     }
     @items << item
