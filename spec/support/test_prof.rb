7a9a555409313371b52fd240ae1132732f62e979
Mika Hel <mikael@zoolutions.se>
2019-04-28 01:45:03 +0200

    Update appraisals

--- spec/support/test_prof.rb
+++ spec/support/test_prof.rb
@@ -4,7 +4,7 @@ require "test_prof"
 
 TestProf.configure do |config|
   # the directory to put artifacts (reports) in ('tmp/test_prof' by default)
-  config.output_dir = 'tmp/test_prof'
+  config.output_dir = "tmp/test_prof"
 
   # use unique filenames for reports (by simply appending current timestamp)
   config.timestamps = true
