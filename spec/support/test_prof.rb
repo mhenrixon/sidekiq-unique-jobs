4da1ca33d221aac70742356cc94fbda1fb29f57e
Mika Hel <mikael@zoolutions.se>
2019-04-28 02:01:42 +0200

    Allow test-prof to not load

--- spec/support/test_prof.rb
+++ spec/support/test_prof.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-require "test_prof"
+begin
+  require "test_prof"
 
-TestProf.configure do |config|
-  # the directory to put artifacts (reports) in ('tmp/test_prof' by default)
-  config.output_dir = "tmp/test_prof"
+  TestProf.configure do |config|
+    # the directory to put artifacts (reports) in ('tmp/test_prof' by default)
+    config.output_dir = "tmp/test_prof"
 
-  # use unique filenames for reports (by simply appending current timestamp)
-  config.timestamps = true
+    # use unique filenames for reports (by simply appending current timestamp)
+    config.timestamps = true
 
-  # color output
-  config.color = true
+    # color output
+    config.color = true
+  end
+
+rescue LoadError
+  # Do nothing, we don't have test_prof
 end
