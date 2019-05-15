3d28e6953196fe7b9f7d5462e0687d3ffd28c671
Mika Hel <mikael@zoolutions.se>
2019-05-15 21:12:48 +0200

    Fix broken spec

---
+++ lib/sidekiq/logging.rb
@@ -0,0 +1,17 @@
+require 'time'
+require 'logger'
+require 'fcntl'
+
+module Sidekiq::Logging
+  def self.with_context(msg)
+    Thread.current[:sidekiq_context] = []
+    Thread.current[:sidekiq_context] << msg
+    yield
+  ensure
+    Thread.current[:sidekiq_context] = {}
+  end
+
+  def logger
+    Sidekiq::Logging.logger
+  end
+end
