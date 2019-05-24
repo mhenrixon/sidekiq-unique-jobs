78ea163e834b86222d321ddc7d249edb22528c89
Mika Hel <mikael@zoolutions.se>
2019-05-24 10:17:55 +0200

    Fix flaky time helper test

--- spec/sidekiq_unique_jobs/web/helpers_spec.rb
+++ spec/sidekiq_unique_jobs/web/helpers_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe SidekiqUniqueJobs::Web::Helpers do
 
     let(:frozen_time) { Time.new(1982, 6, 8, 14, 15, 34) }
     let(:time)        { Time.now.to_f }
+    let(:stamp)       { Time.now.getutc.iso8601 }
 
     before { Timecop.freeze(frozen_time) }
 
@@ -16,7 +17,7 @@ RSpec.describe SidekiqUniqueJobs::Web::Helpers do
 
     it "returns relative time html" do
       expect(safe_relative_time).to eq(<<~HTML.chop)
-        <time class="ltr" dir="ltr" title="1982-06-08T12:15:34Z" datetime="1982-06-08T12:15:34Z">#{Time.now}</time>
+        <time class="ltr" dir="ltr" title="#{stamp}" datetime="#{stamp}">#{Time.now}</time>
       HTML
     end
   end
