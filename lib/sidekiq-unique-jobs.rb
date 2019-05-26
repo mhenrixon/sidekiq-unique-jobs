c3403a0edad9a64cd43aa51997e5b668ef1e6a7c
Mika Hel <mikael@zoolutions.se>
2019-05-26 12:33:32 +0200

    Adds some notes about what to do with startup

--- lib/sidekiq-unique-jobs.rb
+++ lib/sidekiq-unique-jobs.rb
@@ -6,6 +6,15 @@ SidekiqUniqueJobs::Middleware.configure
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
+    # 1. Check for key uniquejobs:version
+    # 2. Copy uniquejobs:version to uniquejobs:prev_version when key exists
+    # 3. Set uniquejobs:version to the current gem version
+    # 4. Register the right conversion script
+  end
+
+  # TODO: Check whether to use heartbeat or a separate threadpool
+  config.on(:heartbeat) do
+    # 1. Run orphan cleanup script
   end
 end
 
