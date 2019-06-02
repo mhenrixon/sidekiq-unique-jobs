7f3fcaf8fd30de6236a369ea42fd01f0e12baf54
Mika Hel <mikael@zoolutions.se>
2019-06-02 18:45:51 +0200

    Guard against slow tests

--- spec/workers/until_expired_job_spec.rb
+++ spec/workers/until_expired_job_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe UntilExpiredJob do
       it "sets keys to expire as per configuration" do
         lock_ttl = described_class.get_sidekiq_options["lock_ttl"]
         unique_keys.all? do |key|
-          expect(key).to have_ttl(lock_ttl + 60)
+          expect(key).to have_ttl(lock_ttl + 60).within(10)
         end
       end
     end
@@ -77,7 +77,7 @@ RSpec.describe UntilExpiredJob do
       it "sets keys to expire as per configuration" do
         lock_ttl = described_class.get_sidekiq_options["lock_ttl"]
         unique_keys.all? do |key|
-          expect(key).to have_ttl(lock_ttl)
+          expect(key).to have_ttl(lock_ttl).within(10)
         end
       end
     end
