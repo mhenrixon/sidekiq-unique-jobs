2a252e4e905cba1982b0c7b46598362c55a6bc67
mhenrixon <mikael@mhenrixon.com>
2020-03-22 12:11:37 +0100

    Begin adding coverage for keys and expiration

--- spec/sidekiq_unique_jobs/lua/lock_spec.rb
+++ spec/sidekiq_unique_jobs/lua/lock_spec.rb
@@ -3,21 +3,38 @@
 RSpec.describe "lock.lua" do
   subject(:lock) { call_script(:lock, key.to_a, argv_one) }
 
-  let(:argv_one)       { [job_id_one, lock_ttl, lock_type, lock_limit] }
-  let(:argv_two)       { [job_id_two, lock_ttl, lock_type, lock_limit] }
-  let(:job_id_one)     { "job_id_one" }
-  let(:job_id_two)     { "job_id_two" }
-  let(:lock_type)      { :until_executed }
-  let(:digest)         { "uniquejobs:digest" }
-  let(:key)            { SidekiqUniqueJobs::Key.new(digest) }
-  let(:redlock)        { SidekiqUniqueJobs::Lock.new(digest) }
-  let(:queued)         { redlock.queued }
-  let(:primed)         { redlock.primed }
-  let(:locked)         { redlock.locked }
-  let(:lock_ttl)       { nil }
-  let(:locked_jid)     { job_id_one }
-  let(:now_f)          { SidekiqUniqueJobs.now_f }
-  let(:lock_limit)     { 1 }
+  let(:argv_one)   { [job_id_one, lock_ttl, lock_type, lock_limit] }
+  let(:argv_two)   { [job_id_two, lock_ttl, lock_type, lock_limit] }
+  let(:job_id_one) { "job_id_one" }
+  let(:job_id_two) { "job_id_two" }
+  let(:lock_type)  { :until_executed }
+  let(:digest)     { "uniquejobs:digest" }
+  let(:key)        { SidekiqUniqueJobs::Key.new(digest) }
+  let(:redlock)    { SidekiqUniqueJobs::Lock.new(digest) }
+  let(:queued)     { redlock.queued }
+  let(:primed)     { redlock.primed }
+  let(:locked)     { redlock.locked }
+  let(:lock_ttl)   { nil }
+  let(:locked_jid) { job_id_one }
+  let(:now_f)      { SidekiqUniqueJobs.now_f }
+  let(:lock_limit) { 1 }
+
+  context "when lock_type == :until_expired" do
+    let(:lock_type) { :until_expired }
+    let(:lock_ttl)  { 50_000 }
+
+    before do
+      call_script(:queue, key.to_a, argv_one)
+      rpoplpush(key.queued, key.primed)
+    end
+
+    it "expires the correct keys" do
+      expect { lock }.to change { zcard(key.changelog) }.by(1)
+
+      expect(ttl(key.digest)).to eq(lock_ttl / 1000)
+      expect(ttl(key.locked)).to eq(lock_ttl / 1000)
+    end
+  end
 
   context "when not queued" do
     it "updates Redis correctly" do
@@ -25,10 +42,10 @@ RSpec.describe "lock.lua" do
 
       expect(lock).to eq(job_id_one)
 
-      expect(queued.count).to be == 0
-      expect(primed.count).to be == 0
+      expect(queued.count).to eq(0)
+      expect(primed.count).to eq(0)
 
-      expect(locked.count).to be == 1
+      expect(locked.count).to eq(1)
       expect(locked.entries).to match_array([job_id_one])
       expect(locked[job_id_one].to_f).to be_within(0.5).of(now_f)
     end
@@ -44,10 +61,10 @@ RSpec.describe "lock.lua" do
 
       expect(lock).to eq(job_id_one)
 
-      expect(queued.count).to be == 0
-      expect(primed.count).to be == 0
+      expect(queued.count).to eq(0)
+      expect(primed.count).to eq(0)
 
-      expect(locked.count).to be == 1
+      expect(locked.count).to eq(1)
       expect(locked.entries).to match_array([job_id_one])
       expect(locked[job_id_one].to_f).to be_within(0.5).of(now_f)
     end
@@ -65,10 +82,10 @@ RSpec.describe "lock.lua" do
       expect(lock).to eq(job_id_one)
       expect(get(key.digest)).to eq(job_id_one)
 
-      expect(queued.count).to be == 0
-      expect(primed.count).to be == 0
+      expect(queued.count).to eq(0)
+      expect(primed.count).to eq(0)
 
-      expect(locked.count).to be == 1
+      expect(locked.count).to eq(1)
       expect(locked.entries).to match_array([job_id_one])
       expect(locked[job_id_one].to_f).to be_within(0.5).of(now_f)
     end
@@ -88,10 +105,10 @@ RSpec.describe "lock.lua" do
         expect(lock).to eq(nil)
         expect(get(key.digest)).to eq(job_id_two)
 
-        expect(queued.count).to be == 0
-        expect(primed.count).to be == 0
+        expect(queued.count).to eq(0)
+        expect(primed.count).to eq(0)
 
-        expect(locked.count).to be == 1
+        expect(locked.count).to eq(1)
         expect(locked.entries).to match_array([job_id_two])
         expect(locked[job_id_two].to_f).to be_within(0.5).of(now_f)
       end
@@ -115,10 +132,10 @@ RSpec.describe "lock.lua" do
         expect(lock).to eq(job_id_one)
         expect(get(key.digest)).to eq(job_id_one)
 
-        expect(queued.count).to be == 0
-        expect(primed.count).to be == 0
+        expect(queued.count).to eq(0)
+        expect(primed.count).to eq(0)
 
-        expect(locked.count).to be == 2
+        expect(locked.count).to eq(2)
         expect(locked.entries).to match_array([job_id_two, job_id_one])
         expect(locked[job_id_two].to_f).to be_within(0.5).of(now_f)
         expect(locked[job_id_one].to_f).to be_within(0.5).of(now_f)
@@ -140,10 +157,10 @@ RSpec.describe "lock.lua" do
       expect(lock).to eq(job_id_one)
       expect(get(key.digest)).to eq(job_id_one)
 
-      expect(queued.count).to be == 0
-      expect(primed.count).to be == 0
+      expect(queued.count).to eq(0)
+      expect(primed.count).to eq(0)
 
-      expect(locked.count).to be == 1
+      expect(locked.count).to eq(1)
       expect(locked.entries).to match_array([job_id_one])
       expect(locked[job_id_one].to_f).to be_within(0.5).of(now_f)
     end
