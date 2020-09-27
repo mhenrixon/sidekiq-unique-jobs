8d7c25607a9c40b5cc6bf19c3551db457ba3fafe
mhenrixon <mikael@mhenrixon.com>
2020-09-27 11:35:14 +0200

    Adds missing coverage

--- spec/sidekiq_unique_jobs/version_check_spec.rb
+++ spec/sidekiq_unique_jobs/version_check_spec.rb
@@ -1,13 +1,58 @@
 # frozen_string_literal: true
 
 RSpec.describe SidekiqUniqueJobs::VersionCheck do
-  let(:version_check) { described_class.new(version, constraint) }
+  describe ".satisfied?" do
+    subject(:satisfied?) { described_class.satisfied?(version, constraint) }
+
+    let(:version)       { "4.0.1" }
+    let(:constraint)    { ">= 4.0.0" }
+
+    context "when given one constraint" do
+      it { is_expected.to eq(true) }
+    end
+
+    context "when given dual constraints" do
+      let(:constraint) { ">= 3.2.5 <= 4.2.1" }
+
+      it { is_expected.to eq(true) }
+    end
+
+    context "when not satisfied" do
+      let(:constraint) { ">= 5.0.0" }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
+  describe ".unfulfilled?" do
+    subject(:unfulfilled?) { described_class.unfulfilled?(version, constraint) }
+
+    let(:version)    { "4.0.1" }
+    let(:constraint) { ">= 4.0.0" }
+
+    context "when given one constraint" do
+      it { is_expected.to eq(false) }
+    end
+
+    context "when given dual constraints" do
+      let(:constraint) { ">= 3.2.5 <= 4.2.1" }
+
+      it { is_expected.to eq(false) }
+    end
+
+    context "when not satisfied" do
+      let(:constraint) { ">= 5.0.0" }
+
+      it { is_expected.to eq(true) }
+    end
+  end
 
   describe "#satisfied?" do
     subject(:satisfied?) { version_check.satisfied? }
 
-    let(:version)    { "4.0.1" }
-    let(:constraint) { ">= 4.0.0" }
+    let(:version_check) { described_class.new(version, constraint) }
+    let(:version)       { "4.0.1" }
+    let(:constraint)    { ">= 4.0.0" }
 
     context "when given one constraint" do
       it { is_expected.to eq(true) }
