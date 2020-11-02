7c104aa68ee512f45a49cb1ff27d6efda38c865c
mhenrixon <mikael@mhenrixon.com>
2020-11-02 21:26:09 +0100

    Clean up the run keys when given a run key

--- redis/delete_by_digest.lua
+++ redis/delete_by_digest.lua
@@ -5,19 +5,24 @@ local exists_key        = KEYS[3]
 local grabbed_key       = KEYS[4]
 local available_key     = KEYS[5]
 local version_key       = KEYS[6]
-local run_exists_key    = KEYS[7]
-local run_grabbed_key   = KEYS[8]
-local run_available_key = KEYS[9]
-local run_version_key   = KEYS[10]
+local run_key           = KEYS[7]
+local run_exists_key    = KEYS[8]
+local run_grabbed_key   = KEYS[9]
+local run_available_key = KEYS[10]
+local run_version_key   = KEYS[11]
 
 local count = redis.call('SREM', unique_keys, unique_digest)
 redis.call('DEL', exists_key)
 redis.call('DEL', grabbed_key)
 redis.call('DEL', available_key)
 redis.call('DEL', version_key)
-redis.call('DEL', run_exists_key)
-redis.call('DEL', run_grabbed_key)
-redis.call('DEL', run_available_key)
-redis.call('DEL', run_version_key)
+
+local result, index = run_key:gsub(':RUN:RUN', "")
+if index == 0 then
+  redis.call('DEL', run_exists_key)
+  redis.call('DEL', run_grabbed_key)
+  redis.call('DEL', run_available_key)
+  redis.call('DEL', run_version_key)
+end
 
 return count
