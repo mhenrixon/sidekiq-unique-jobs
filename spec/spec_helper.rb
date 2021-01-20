bee36fe380e6642ed7ff183c1c2dd19f5ef12eb2
mhenrixon <mikael@mhenrixon.com>
2021-01-20 09:43:22 +0100

    Configure the middleware for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,34 @@ Redis.exists_returns_integer = true if Redis.respond_to?(:exists_returns_integer
 LOGLEVEL = ENV.fetch("LOGLEVEL", "ERROR").upcase
 ORIGINAL_SIDEKIQ_OPTIONS = Sidekiq.default_worker_options
 
+Sidekiq.default_worker_options = {
+  backtrace: true,
+  retry: true,
+}
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_URL"], driver: :hiredis }
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  config.error_handlers << ->(ex, ctx_hash) { p ex, ctx_hash }
+  config.death_handlers << lambda do |job, _ex|
+    digest = job["lock_digest"]
+    SidekiqUniqueJobs::Digests.delete_by_digest(digest) if digest
+  end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"], driver: :hiredis }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+end
+
+
 SidekiqUniqueJobs.configure do |config|
   config.logger.level = Logger.const_get(LOGLEVEL)
   config.debug_lua    = %w[1 true].include?(ENV["DEBUG_LUA"])
