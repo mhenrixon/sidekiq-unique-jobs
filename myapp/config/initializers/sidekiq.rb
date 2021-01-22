e5ed9d621012f61ed369da9a2b09d8639c7aecd6
mhenrixon <mikael@mhenrixon.com>
2021-01-22 23:04:06 +0100

    Create a more sane initializer

--- myapp/config/initializers/sidekiq.rb
+++ myapp/config/initializers/sidekiq.rb
@@ -13,12 +13,6 @@ Sidekiq.default_worker_options = {
 }
 
 Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    chain.add SidekiqUniqueJobs::Middleware::Client
-  end
-end
-
-Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"], driver: :hiredis }
 
   config.client_middleware do |chain|
@@ -38,21 +32,18 @@ Sidekiq.configure_server do |config|
   end
 
   SidekiqUniqueJobs::Server.configure(config)
-
-  config.error_handlers << ->(ex, ctx_hash) { p ex, ctx_hash }
 end
 
 Sidekiq.logger       = Sidekiq::Logger.new($stdout)
 Sidekiq.logger.level = :debug
-Sidekiq.log_format = :json if Sidekiq.respond_to?(:log_format)
+Sidekiq.log_format   = :json if Sidekiq.respond_to?(:log_format)
 SidekiqUniqueJobs.configure do |config|
-  config.debug_lua       = true
-  config.lock_info       = true
-  config.logger          = Sidekiq.logger
-  config.max_history     = 10_000
-  config.reaper          = :lua
-  config.reaper_count    = 100
-  config.reaper_interval = 10
-  config.reaper_timeout  = 5
+  config.debug_lua       = false # true for debugging
+  config.lock_info       = false # true for debugging
+  config.max_history     = 1000  # keeps n number of changelog entries
+  config.reaper          = :ruby # also :lua but that will lock while cleaning
+  config.reaper_count    = 1000  # Reap maximum this many orphaned locks
+  config.reaper_interval = 10    # Reap every 10 seconds
+  config.reaper_timeout  = 5     # Give the reaper 5 seonds to finish
 end
 Dir[Rails.root.join("app", "workers", "**", "*.rb")].sort.each { |worker| require worker }
