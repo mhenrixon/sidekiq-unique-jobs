mhenrixon/sidekiq-unique-jobs

commit 76cfc922e7a0c9771a65887bb78f5b85dbfd6204

pass lock timeout to primed_async (#624)
relates to https://github.com/mhenrixon/sidekiq-unique-jobs/issues/617

There is an issue where an integer `config.timeout` was being
respected in `pop_queued`, but the promise in `primed_async` was only
waiting the `config.ttl` plus the drift.

This means that high latency redis connections will return a `nil`
`primed_jid` and while_executing jobs will get dropped.

The proposed solution is to ensure that `config.timeout` gets passed in
as the `wait` argument to the bound `method(:primed_async)`, which
allows the `Concurrent::Promises` to timeout after the same duration
(plus drift) as the `brpoplpush` call.

The issue triage and approach is documented in:

  - https://github.com/mhenrixon/sidekiq-unique-jobs/issues/617#issuecomment-882826710
  - https://github.com/mhenrixon/sidekiq-unique-jobs/issues/617#issuecomment-883458295
  - https://github.com/mhenrixon/sidekiq-unique-jobs/issues/617#issuecomment-883488756

lib/sidekiq_unique_jobs/locksmith.rb
@@ -102,7 +102,7 @@ module SidekiqUniqueJobs
       raise SidekiqUniqueJobs::InvalidArgument, "#execute needs a block" unless block
 
       redis(redis_pool) do |conn|
-        lock!(conn, method(:primed_async), &block)
+        lock!(conn, method(:primed_async), config.timeout, &block)
       end
     end
 

spec/sidekiq_unique_jobs/locksmith_spec.rb
@@ -288,6 +288,13 @@ RSpec.describe SidekiqUniqueJobs::Locksmith do
   context "when lock_timeout is 1" do
     let(:lock_timeout) { 1 }
 
+    let(:slow_brpoplpush) do
+      proc do
+        sleep(0.75)
+        locksmith_one.job_id
+      end
+    end
+
     it "blocks other locks" do
       did_we_get_in = false
 
@@ -298,6 +305,18 @@ RSpec.describe SidekiqUniqueJobs::Locksmith do
       end
 
       expect(did_we_get_in).to be false
+    end
+
+    it "waits for brpoplpush when resolving the promise" do
+      allow(locksmith_one).to receive(:brpoplpush).with(anything, 1, &slow_brpoplpush)
+
+      did_we_get_in = false
+      locksmith_one.execute do
+        did_we_get_in = true
+      end
+
+      expect(locksmith_one).to have_received(:brpoplpush)
+      expect(did_we_get_in).to be true
     end
   end
 

commit 425c1a51922b83979473c60da14960200b1d2eed

Fix UntilAndWhileExecuting
Locking mechanism was broken in #624.

lib/sidekiq_unique_jobs/locksmith.rb
@@ -102,7 +102,7 @@ module SidekiqUniqueJobs
       raise SidekiqUniqueJobs::InvalidArgument, "#execute needs a block" unless block
 
       redis(redis_pool) do |conn|
-        lock!(conn, method(:primed_async), config.timeout, &block)
+        lock!(conn, method(:primed_async), &block)
       end
     end
 
@@ -179,6 +179,9 @@ module SidekiqUniqueJobs
     #
     # Used to reduce some duplication from the two methods
     #
+    # @see lock
+    # @see execute
+    #
     # @param [Sidekiq::RedisConnection, ConnectionPool] conn the redis connection
     # @param [Method] primed_method reference to the method to use for getting a primed token
     #
@@ -238,7 +241,7 @@ module SidekiqUniqueJobs
     def primed_async(conn, wait = nil, &block)
       primed_jid = Concurrent::Promises
                    .future(conn) { |red_con| pop_queued(red_con, wait) }
-                   .value(add_drift(wait || config.ttl))
+                   .value(add_drift(wait || config.timeout))
 
       handle_primed(primed_jid, &block)
     end

spec/sidekiq_unique_jobs/locksmith_spec.rb
@@ -29,6 +29,8 @@ RSpec.describe SidekiqUniqueJobs::Locksmith do
       "queue" => queue,
     }
   end
+  let(:key_one)  { locksmith_one.key }
+  let(:key_two)  { locksmith_two.key }
   let(:item_two) { item_one.merge("jid" => jid_two) }
 
   describe "#to_s" do
@@ -288,13 +290,6 @@ RSpec.describe SidekiqUniqueJobs::Locksmith do
   context "when lock_timeout is 1" do
     let(:lock_timeout) { 1 }
 
-    let(:slow_brpoplpush) do
-      proc do
-        sleep(0.75)
-        locksmith_one.job_id
-      end
-    end
-
     it "blocks other locks" do
       did_we_get_in = false
 
@@ -308,7 +303,10 @@ RSpec.describe SidekiqUniqueJobs::Locksmith do
     end
 
     it "waits for brpoplpush when resolving the promise" do
-      allow(locksmith_one).to receive(:brpoplpush).with(anything, 1, &slow_brpoplpush)
+      allow(locksmith_one).to receive(:brpoplpush) do
+        sleep(0.75)
+        jid_one
+      end
 
       did_we_get_in = false
       locksmith_one.execute do