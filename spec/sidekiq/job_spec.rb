f04091597f602b803039b9f5742adbfd7e070cf9
mhenrixon <mikael@mhenrixon.com>
2023-04-13 18:35:30 +0200

    chore(lint): lint'em real good

--- spec/sidekiq/job_spec.rb
+++ spec/sidekiq/job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Sidekiq::Job do
   describe "#clear_all" do
     it "unlocks all unique locks" do
       expect(UntilAndWhileExecutingJob.perform_async).not_to be_nil
-      Sidekiq::Job.clear_all
+      described_class.clear_all
       expect(UntilAndWhileExecutingJob.perform_async).not_to be_nil
     end
   end
