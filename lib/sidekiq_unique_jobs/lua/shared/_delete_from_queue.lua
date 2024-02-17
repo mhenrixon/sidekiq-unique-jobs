a25420bb39f57c0c10e553d55a7f507bf9e190cb
mhenrixon <mikael@mhenrixon.com>
2024-02-17 08:49:20 +0200

    fix(perf): break out all way on found

--- lib/sidekiq_unique_jobs/lua/shared/_delete_from_queue.lua
+++ lib/sidekiq_unique_jobs/lua/shared/_delete_from_queue.lua
@@ -1,22 +1,22 @@
 local function delete_from_queue(queue, digest)
-  local per    = 50
-  local total  = redis.call("LLEN", queue)
-  local index  = 0
+  local per = 50
+  local total = redis.call("LLEN", queue)
   local result = nil
 
-  while (index < total) do
-    local items = redis.call("LRANGE", queue, index, index + per -1)
+  for index = 0, total, per do
+    local items = redis.call("LRANGE", queue, index, index + per - 1)
     if #items == 0 then
       break
     end
-    for _, item in pairs(items) do
+    for _, item in ipairs(items) do
       if string.find(item, digest) then
         redis.call("LREM", queue, 1, item)
         result = item
         break
       end
     end
-    index = index + per
+    if result then break end
   end
+
   return result
 end
